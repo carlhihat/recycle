399740072da40d25561ccc0c2a05a8e6ae3cf9e9
koettpaj <carlkernell@gmail.com>
2019-12-02 12:29:51 +0100

    yo

---
+++ Assets/Resources/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject testObj;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit rayhit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out rayhit)){
+                GameObject hitThing = rayhit.collider.gameObject;
+                Renderer rend = hitThing.GetComponent<Renderer>();
+                Material material = new Material(Shader.Find("Standard"));
+                material.color = Color.green;
+                rend.material = material;
+
+            }
+        }
+    }
+}
