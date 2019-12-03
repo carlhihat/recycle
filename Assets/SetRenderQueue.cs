6a16287f9a0fc077c69972efd510367a413347e0
koettpaj <carlkernell@gmail.com>
2019-12-03 21:27:32 +0100

    massor grejer

---
+++ Assets/SetRenderQueue.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[AddComponentMenu("Rendering/SetRenderQueue")]
+
+public class SetRenderQueue : MonoBehaviour
+{
+    public GameObject testObj;
+    [SerializeField]
+    protected int[] m_queues = new int[] { 3000 };
+
+    protected void Awake()
+    {
+        Renderer renderer = testObj.GetComponent<Renderer>();
+        Material[] materials = renderer.materials;
+        for (int i = 0; i < materials.Length && i < m_queues.Length; ++i)
+        {
+            materials[i].renderQueue = m_queues[i];
+        }
+    }
+}
